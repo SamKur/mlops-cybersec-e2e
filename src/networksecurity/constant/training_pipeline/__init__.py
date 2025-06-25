c940b78ceca5d1dd5f150b2863edd2e57e07d3da
Susamay Kumbhakar <susamay.sk@gmail.com>
2025-06-25 21:29:50 +0530

    working with components constants entity

---
+++ src/networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,9 @@
+import sys, os
+import numpy as np
+import pandas as pd
+
+"""
+This module contains constants used in the training pipeline of the network security project.
+Data Ingestion related constants start with `DATA_INGESTION_`
+"""
+
